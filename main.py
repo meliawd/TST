fbb7d3b7bb0a1ced5940e132b933e776864b2d71
amelia18220043 <74282979+meliawd@users.noreply.github.com>
2022-11-22 01:55:42 +0700

    Update main.py
    main.py

--- main.py
+++ main.py
@@ -5,6 +5,30 @@ import json
 
 app = FastAPI()
 
-class mahasiswa(BaseModel):
-    nim: int
-    nama: str
+class beasiswa(BaseModel):
+    Beasiswa : str
+    Indeks : float
+    UKT : int
+    Gaji : int
+
+@app.post("/")
+async def add_beasiswa(b: beasiswa):
+    list_beasiswa = []
+
+    with open('beasiswa.json', 'r') as f:
+        list_beasiswa = json.load(f)['beasiswa']
+
+    new_beasiswa = {
+        "Beasiswa" : b.Beasiswa,
+        "Indeks" : b.Indeks,
+        "UKT" : b.UKT,
+        "Gaji" : b.Gaji
+        }
+
+    list_beasiswa.append(new_beasiswa)
+
+    output = {'beasiswa':list_beasiswa}
+    with open('beasiswa.json', 'w') as f:
+        json.dump(output, f)
+
+    return "Data beasiswa baru: " +str(new_beasiswa)+ " telah diterima"
